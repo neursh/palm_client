46c0788f436982211f6449577ab6412249c5b0b2
Neurs <hoanghuutrung431@gmail.com>
2024-03-07 17:07:51 +0700

    📝🛠️ Spelling mistake + improve

---
+++ lib/client_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+import "package:socket_io_client/socket_io_client.dart" as sio;
+
+enum KeyInput { left, right, shiftf5, f5, esc }
+
+class ClientProvider extends ChangeNotifier {
+  bool authorized = false;
+  bool connection = false;
+  sio.Socket? socket;
+
+  connect(String ip, int port, String pin) {
+    connection = true;
+
+    socket = sio.io(
+      "http://$ip:$port",
+      sio.OptionBuilder()
+          .enableForceNew()
+          .setTransports(["websocket"])
+          .setExtraHeaders({"PIN": pin})
+          .disableAutoConnect()
+          .disableReconnection()
+          .build(),
+    );
+
+    socket!.on("authorized", (data) {
+      authorized = true;
+      notifyListeners();
+    });
+
+    socket!.on("disconnect", (data) {
+      disconnect();
+      notifyListeners();
+    });
+
+    socket!.connect();
+  }
+
+  sendKey(KeyInput input) {
+    socket!.emit("input", input.name);
+  }
+
+  disconnect() {
+    socket!.dispose();
+    socket = null;
+    connection = false;
+    authorized = false;
+    notifyListeners();
+  }
+}
